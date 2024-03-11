af05a844b3361529351ae03670997ac96dac9239
LorenzoMonteforte92 <l.monteforte@yahoo-it>
2024-03-11 16:12:49 +0100

    feat: snack 4 finished

--- snack4/JS/script.js
+++ snack4/JS/script.js
@@ -1,13 +1,17 @@
 //Calcola la somma e la media dei primi 10 numeri.
 
 //creo array con 10 numeri
-num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-console.log(num)
+let arrayNum = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+console.log(arrayNum)
 // somma numeri
-totalSum = 0
-for(let i = 0; i < num.length; i++ ){
+let totalSum = 0
+for(let i = 0; i < arrayNum.length; i++ ){
     //scorri i numeri nell'array e addizionali alla variabile totalSum
-    totalSum += num[i]
+    totalSum += arrayNum[i]
 }
-
 console.log(totalSum);
+
+//calcola la media dei numeri
+console.log(arrayNum.length);
+let averageNum = totalSum / arrayNum.length;
+console.log(averageNum);
