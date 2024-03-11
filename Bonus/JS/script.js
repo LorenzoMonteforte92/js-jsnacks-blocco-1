2b49b06753fada594b18baebee3c0f80e61e624e
LorenzoMonteforte92 <l.monteforte@yahoo-it>
2024-03-11 16:29:35 +0100

    feat: create array

--- Bonus/JS/script.js
+++ Bonus/JS/script.js
@@ -1,1 +1,14 @@
 // Dato un array contenente 10 numeri, stampare in console il numero più grande.
+//genero numeri random per array
+// let arrayNum = []
+// for(let i = 0; i < 10; i++){
+//     randomNum = Math.floor(Math.random() * 100) + 1;
+//     console.log(randomNum);
+//     arrayNum.push(randomNum);
+
+// }
+// console.log(arrayNum);
+
+//scrivo array
+num = [91, 60, 75, 1, 52, 34, 89, 28, 24, 49]
+
