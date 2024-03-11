f126ae4fbf56451a2fb1c3a9cc9e23f63ae42a5e
LorenzoMonteforte92 <l.monteforte@yahoo-it>
2024-03-11 15:35:28 +0100

    feat: add external variable

---
+++ snack2/JS/script.js
@@ -0,0 +1,5 @@
+//Il software deve chiedere per 5 volte all’utente di inserire un numero.
+//Il programma stampa la somma di tutti i numeri inseriti.
+
+//dichiaro la variabile somma all'esterno
+let sum = 0
