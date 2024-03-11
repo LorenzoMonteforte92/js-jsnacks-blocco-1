638fd0bc816b6984de436298c99d761b1839a8b1
LorenzoMonteforte92 <l.monteforte@yahoo-it>
2024-03-11 15:42:45 +0100

    feat: add for cycle

---
+++ snack3/JS/script.js
@@ -0,0 +1,11 @@
+//Crea un array vuoto.
+//Chiedi per 6 volte all’utente di inserire un numero, 
+//se è dispari inseriscilo nell’array.
+
+//array vuoto
+userNum = []
+
+//chiedere numeri a utente
+for(let i = 0; i < 6; i++){
+    
+}
