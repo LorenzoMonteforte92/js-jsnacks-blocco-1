19d4c8f18b16cc370ca5ab55f1d67f2063eb5658
LorenzoMonteforte92 <l.monteforte@yahoo-it>
2024-03-11 16:01:58 +0100

    feat: add sum of numbers

--- snack4/JS/script.js
+++ snack4/JS/script.js
@@ -3,3 +3,11 @@
 //creo array con 10 numeri
 num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 console.log(num)
+// somma numeri
+totalSum = 0
+for(let i = 0; i < num.length; i++ ){
+    //scorri i numeri nell'array e addizionali alla variabile totalSum
+    totalSum += num[i]
+}
+
+console.log(totalSum);
