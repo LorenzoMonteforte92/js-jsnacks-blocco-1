894bb3a2823b68b79fe5324eec9e60079f39edd0
LorenzoMonteforte92 <l.monteforte@yahoo-it>
2024-03-11 15:37:54 +0100

    feat: add for cyrcle

--- snack2/JS/script.js
+++ snack2/JS/script.js
@@ -3,3 +3,9 @@
 
 //dichiaro la variabile somma all'esterno
 let sum = 0
+//per cinque volte chiedo all'utente un numero
+for(let i = 0; i < 5; i++){
+    userNumber = parseInt(prompt(`Scrivi un numero`));
+    //sommo ogni numero inserito dall'utente e lo inserisco nella variabile sum
+    sum += userNumber;
+}
